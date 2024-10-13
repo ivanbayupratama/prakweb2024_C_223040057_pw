d88d67a9951f6ea44403a937cd15fa12b601c24a
Ivan Bayu Pratama <121966880+ivanbayupratama@users.noreply.github.com>
2024-10-13 12:29:16 +0700

    Tugas-3 OOP

---
+++ Tugas3/OOP/constant.php
@@ -0,0 +1,9 @@
+<?php
+
+define('NAMA', 'Ivan Bayu Pratama');
+echo NAMA;
+
+echo "<br>";
+
+const UMUR = 21;
+echo UMUR;
---
+++ Tugas3/OOP/constructor.php
@@ -0,0 +1,30 @@
+<?php
+
+class Produk
+{
+    public  $judul,
+        $penulis,
+        $penerbit,
+        $harga;
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function __construct($judul, $penulis, $penerbit, $harga)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+}
+
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
